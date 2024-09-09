fff020f85c98a4f81ab82443c0da8f675d1d2d80
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-09-09 21:37:41 +0200

    movie details

--- frontend/src/components/MovieDetail.jsx
+++ frontend/src/components/MovieDetail.jsx
@@ -32,14 +32,14 @@ const MovieDetail = () => {
     );
 
     return (
-        <div className="py-20 px-4">
+        <div className="py-10 px-4 lg:px-20">
             {/* Backdrop Image */}
             {movieDetail.movie.belongs_to_collection?.poster_path && (
                 <div className="relative mb-8">
                     <img
                         src={`https://image.tmdb.org/t/p/w500${movieDetail.movie.belongs_to_collection.poster_path}`}
                         alt={movieDetail.movie.title || "Backdrop"}
-                        className="w-full h-96 object-cover rounded-lg"
+                        className="w-full h-80 object-cover rounded-lg"
                     />
                     <div className="absolute inset-0 bg-black bg-opacity-40"></div>
                 </div>
@@ -50,30 +50,29 @@ const MovieDetail = () => {
                 {movieDetail.movie.title || "Title not available"}
             </h1>
 
-            <div className="flex flex-col md:flex-row items-center">
+            <div className="flex flex-col lg:flex-row lg:items-start">
                 {/* Poster Image */}
                 {movieDetail.movie.poster_path && (
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500${movieDetail.movie.poster_path}`}
-                        alt={movieDetail.movie.title || "Poster"}
-                        className="w-full md:w-1/3 h-96 object-cover rounded-lg mb-4"
-                    />
+                    <div className="w-full lg:w-1/3 mb-4 lg:mb-0">
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${movieDetail.movie.poster_path}`}
+                            alt={movieDetail.movie.title || "Poster"}
+                            className="w-full h-auto object-cover rounded-lg"
+                        />
+                    </div>
                 )}
-                <div className="md:ml-8 mt-8 md:mt-0">
+
+                <div className="lg:ml-8 mt-8 lg:mt-0 w-full lg:w-2/3">
                     {/* Original Title */}
                     <div className="mt-4">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Original Title
-                        </h3>
+                        <h3 className="text-xl font-semibold text-black italic">Original Title</h3>
                         <p className="text-gray-600 italic">
                             {movieDetail.movie.original_title || "Original title not available"}
                         </p>
                     </div>
 
                     {/* Overview */}
-                    <h2 className="text-2xl font-semibold mb-4 text-black italic mt-6">
-                        Overview
-                    </h2>
+                    <h2 className="text-2xl font-semibold mb-4 text-black italic mt-6">Overview</h2>
                     <p className="text-gray-600 italic">
                         {movieDetail.movie.overview || "Overview not available"}
                     </p>
@@ -81,9 +80,7 @@ const MovieDetail = () => {
                     {/* Homepage */}
                     {movieDetail.movie.homepage && (
                         <div className="mt-4">
-                            <h3 className="text-xl font-semibold text-black italic">
-                                Homepage
-                            </h3>
+                            <h3 className="text-xl font-semibold text-black italic">Homepage</h3>
                             <a
                                 href={movieDetail.movie.homepage}
                                 target="_blank"
@@ -97,9 +94,7 @@ const MovieDetail = () => {
 
                     {/* Release Date */}
                     <div className="mt-6">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Release Date
-                        </h3>
+                        <h3 className="text-xl font-semibold text-black italic">Release Date</h3>
                         <p className="text-gray-600 italic">
                             {movieDetail.movie?.release_date || "Release date not available"}
                         </p>
@@ -107,9 +102,7 @@ const MovieDetail = () => {
 
                     {/* Rating */}
                     <div className="mt-4">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Rating
-                        </h3>
+                        <h3 className="text-xl font-semibold text-black italic">Rating</h3>
                         <p className="text-gray-600 italic">
                             {movieDetail.movie.vote_average !== undefined ? movieDetail.movie.vote_average : "N/A"}
                         </p>
@@ -117,9 +110,7 @@ const MovieDetail = () => {
 
                     {/* Vote Count */}
                     <div className="mt-4">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Vote Count
-                        </h3>
+                        <h3 className="text-xl font-semibold text-black italic">Vote Count</h3>
                         <p className="text-gray-600 italic">
                             {movieDetail.movie?.vote_count !== undefined ? movieDetail.movie.vote_count : "N/A"}
                         </p>
@@ -127,9 +118,7 @@ const MovieDetail = () => {
 
                     {/* Popularity */}
                     <div className="mt-4">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Popularity
-                        </h3>
+                        <h3 className="text-xl font-semibold text-black italic">Popularity</h3>
                         <p className="text-gray-600 italic">
                             {movieDetail.movie.popularity !== undefined ? movieDetail.movie?.popularity : "N/A"}
                         </p>
@@ -137,9 +126,7 @@ const MovieDetail = () => {
 
                     {/* Genres */}
                     <div className="mt-4">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Genres
-                        </h3>
+                        <h3 className="text-xl font-semibold text-black italic">Genres</h3>
                         <p className="text-gray-600 italic">
                             {movieDetail.movie.genres && movieDetail.movie.genres.length > 0
                                 ? movieDetail.movie.genres.map((genre) => genre.name).join(', ')
@@ -149,9 +136,7 @@ const MovieDetail = () => {
 
                     {/* Origin Country */}
                     <div className="mt-4">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Origin Country
-                        </h3>
+                        <h3 className="text-xl font-semibold text-black italic">Origin Country</h3>
                         <p className="text-gray-600 italic">
                             {movieDetail.movie.origin_country && movieDetail.movie.origin_country.length > 0
                                 ? movieDetail.movie.origin_country.join(', ')
@@ -161,12 +146,8 @@ const MovieDetail = () => {
 
                     {/* Tagline */}
                     <div className="mt-4">
-                        <h3 className="text-xl font-semibold text-black italic">
-                            Tagline
-                        </h3>
-                        <p className="text-gray-600 italic">
-                            {movieDetail.movie.tagline || "Tagline not available"}
-                        </p>
+                        <h3 className="text-xl font-semibold text-black italic">Tagline</h3>
+                        <p className="text-gray-600 italic">{movieDetail.movie.tagline || "Tagline not available"}</p>
                     </div>
 
                     {/* Budget */}
@@ -180,9 +161,7 @@ const MovieDetail = () => {
                     {/* Status */}
                     {movieDetail.movie.status && (
                         <div className="mt-4">
-                            <h3 className="text-xl font-semibold text-black italic">
-                                Status
-                            </h3>
+                            <h3 className="text-xl font-semibold text-black italic">Status</h3>
                             <p className="text-gray-600 italic">{movieDetail.movie?.status}</p>
                         </div>
                     )}
